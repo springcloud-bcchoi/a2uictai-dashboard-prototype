ddcfabcaa5a44cbf463a6906ff1c2742d5558439
heejeong <devlearning98@gmai.com>
2024-10-22 18:58:04 +0900

    useSelected 훅에서 반환된 값이 null일 경우를 처리하고, 필요한 경우 기본값을 설정

--- components/radio/WrappedRadio.tsx
+++ components/radio/WrappedRadio.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
-import { useSelected } from './RadioContext'; // ChipContext 가져오기
+import { useSelected } from './RadioContext'; // RadioContext 가져오기
 
+const WrappedRadio = (prop: { options: string[]; Component: React.FC<any> }) => {
+    const { options, Component } = prop;
 
-const WrappedRadio = (prop:{ options:string[], Component: React.FC<any> }) => {
-    const {options, Component} = prop;
-    const { selected, setSelected } = useSelected();
+    // useSelected에서 반환된 값을 selectedContext로 받아옵니다.
+    const selectedContext = useSelected();
+    const selected = selectedContext ? selectedContext.selected : null;
+    const setSelected = selectedContext ? selectedContext.setSelected : () => {};
+
     const isAllChipSelected = selected === '';
 
-    const handleOptionClick = (option:string) => {
+    const handleOptionClick = (option: string) => {
         if (option === '전체' || selected === option) {
             // '전체' chip 클릭 시, 다른 모든 chip을 선택 해제
             setSelected('');
         } else {
-                setSelected(option);
-            }
-    };   
+            setSelected(option);
+        }
+    };
 
     return (
         <div style={{
@@ -31,15 +35,15 @@ const WrappedRadio = (prop:{ options:string[], Component: React.FC<any> }) => {
                     ? (isAllChipSelected ? 'filled' : 'outlined')
                     : (selected === option ? 'filled' : 'outlined');
 
-                return(           
-                <Component
-                    key={index}
-                    label={option}
-                    onClick={() => handleOptionClick(option)}
-                    color={color}
-                    variant={variant}
-                    style={{ margin: 4 }}
-                />
+                return (
+                    <Component
+                        key={index}
+                        label={option}
+                        onClick={() => handleOptionClick(option)}
+                        color={color}
+                        variant={variant}
+                        style={{ margin: 4 }}
+                    />
                 );
             })}
         </div>
